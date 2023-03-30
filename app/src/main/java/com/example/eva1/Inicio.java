63d51a8a40df9dcd4c24128d8b15e77daceca83d
Laboratorio 414 Talca 08 <lab406_pc08@cloud.uautonoma.cl>
2023-03-30 09:53:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/eva1/Inicio.java
@@ -0,0 +1,27 @@
+package com.example.eva1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+    Button registrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        registrar = (Button)findViewById(R.id.boton_registro);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(packageContext())
+            }
+        });
+    }
+}
