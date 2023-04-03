5d9f739e2b8a1354d0a463c6aef86c7422cee120
Kuadrao <lucianoonatevenegas@gmail.com>
2023-04-03 15:28:57 -0400

    1.0 alpha

--- app/src/main/java/com/example/eva1/registro.java
+++ app/src/main/java/com/example/eva1/registro.java
@@ -3,15 +3,20 @@ package com.example.eva1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class registro extends AppCompatActivity {
-
+public class registro extends AppCompatActivity implements View.OnClickListener{
+    Button registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        registro =(Button)findViewById(R.id.boton_registro);
+        registro.setOnClickListener(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -27,5 +32,16 @@ public class registro extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.boton_registro:
+                Intent registrar = new Intent(registro.this,lista_compra.class);
+                startActivity(registrar);
+                break;
+
+
+        }
+
     }
 }
