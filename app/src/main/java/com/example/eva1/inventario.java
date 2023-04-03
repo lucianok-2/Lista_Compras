f2e8428494d3347461aaba4a3c4cb58a05bc4c43
Kuadrao <lucianoonatevenegas@gmail.com>
2023-04-02 21:02:40 -0400

    casi final falta boton registro y detalle de productos comprados

--- app/src/main/java/com/example/eva1/inventario.java
+++ app/src/main/java/com/example/eva1/inventario.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.eva1.ListaProductosAdapter;
+import com.example.eva1.R;
+
+import java.util.ArrayList;
 
 public class inventario extends AppCompatActivity {
 
@@ -13,17 +20,35 @@ public class inventario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventario);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Cargar datos de la lista
+        ArrayList<String> listaProductos = new ArrayList<>();
+        listaProductos.add("Producto 1");
+        listaProductos.add("Producto 2");
+        listaProductos.add("Producto 3");
+        listaProductos.add("Producto 4");
+        listaProductos.add("Producto 5");
+
+        // Asociar el adaptador con la lista de productos
+        ListView listView = findViewById(R.id.lista_productos);
+        ListaProductosAdapter adapter = new ListaProductosAdapter(this, listaProductos);
+        listView.setAdapter(adapter);
+
+        // Agregar listener para cuando se hace click en un elemento de la lista
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String productoSeleccionado = listaProductos.get(position);
+            Toast.makeText(getApplicationContext(), "Seleccionaste " + productoSeleccionado, Toast.LENGTH_SHORT).show();
+        });
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         switch(item.getItemId()){
             case android.R.id.home:
                 finish();
                 return true;
-
-
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/eva1/recordatorio.java
+++ app/src/main/java/com/example/eva1/recordatorio.java
@@ -5,25 +5,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class recordatorio extends AppCompatActivity {
+
+    private EditText descripcionEditText;
+    private EditText fechaEditText;
+    private Button agregarRecordatorioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordatorio);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Obtener referencias a los elementos en el layout
+        descripcionEditText = findViewById(R.id.descripcionEditText);
+        fechaEditText = findViewById(R.id.fechaEditText);
+        agregarRecordatorioButton = findViewById(R.id.agregarRecordatorioButton);
+
+        // Agregar listener al botón de agregar recordatorio
+        agregarRecordatorioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener los datos ingresados por el usuario
+                String descripcion = descripcionEditText.getText().toString();
+                String fecha = fechaEditText.getText().toString();
+
+                // Validar que los datos no estén vacíos
+                if (descripcion.isEmpty() || fecha.isEmpty()) {
+                    Toast.makeText(recordatorio.this, "Ingrese todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Obtener la fecha actual y convertirla al formato deseado
+                String fechaActual = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault()).format(Calendar.getInstance().getTime());
+
+                // Mostrar un mensaje con los datos del recordatorio agregado
+                String mensaje = "Recordatorio agregado:\n" +
+                        "Descripción: " + descripcion + "\n" +
+                        "Fecha: " + fecha + "\n" +
+                        "Fecha de creación: " + fechaActual;
+                Toast.makeText(recordatorio.this, mensaje, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         switch(item.getItemId()){
             case android.R.id.home:
                 finish();
                 return true;
-
-
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
